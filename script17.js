93f1b31a111723e8159e14b5fc69f8c7866e92bf
Amira <bcuba@unsa.edu.pe>
2025-11-21 23:17:34 -0500

    Ejercicio 17

---
+++ script17.js
@@ -0,0 +1,17 @@
+const productosOriginales = [
+    { nombre: "Monitor 24'", precio: 150 },
+    { nombre: "Teclado Mecánico", precio: 80 },
+    { nombre: "Ratón Gamer", precio: 40 },
+    { nombre: "Audífonos", precio: 60 }
+];
+const jsonString = JSON.stringify(productosOriginales);
+console.log("Como texto JSON:", jsonString);
+const productosRecuperados = JSON.parse(jsonString);
+const contenedor = document.getElementById("contenedorLista");
+const listaUl = document.createElement("ul");
+productosRecuperados.forEach((producto) => {
+    const item = document.createElement("li");
+    item.textContent = producto.nombre;      
+    listaUl.appendChild(item);         
+});
+contenedor.appendChild(listaUl);
